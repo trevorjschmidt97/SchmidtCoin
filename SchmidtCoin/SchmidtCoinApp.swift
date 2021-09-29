1db3d60d01999b60336564faaefcc8d34cdf6f24
Trevor Schmidt <trevorjschmidt97@gmail.com>
2021-09-29 00:06:27 -0600

    Initial Commit

---
+++ SchmidtCoin/SchmidtCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchmidtCoinApp.swift
+//  SchmidtCoin
+//
+//  Created by Trevor Schmidt on 9/29/21.
+//
+
+import SwiftUI
+
+@main
+struct SchmidtCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
